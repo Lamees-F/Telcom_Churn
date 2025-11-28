e030ca77b68222962553a6b4f44c0417908ae487
rizwan66 <rizwan.nasir@hotmail.com>
2025-11-28 09:20:18 +0100

    Streamlit

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
-## streamlit app where we call the data, vis, and the model
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+from urllib.request import urlopen
+import json
+from copy import deepcopy
+
+
+
+@st.cache_data
+def load_data(path):
+    df = pd.read_csv(path)
+    return df
+
+churn_df_raw = load_data(path = "data/cleaned_telecom_churn_data.csv")
+churn_df = deepcopy(churn_df_raw)
+
+st.title("Telecom Churn Prediction")
+st.header("Customers Info")
+
+#st.table(data=mpg_df)
+if st.checkbox("Show Dataframe"):
+    st.subheader("Dataset:")
+    st.dataframe(data=churn_df)
+
+left_col, middle_col, right_col = st.columns([3,1,1])
+
+
